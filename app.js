5b4f53f4bcebf4ede45a10d5c7b025ad2d670368
S567536 <S567536@nwmissouri.edu>
2023-11-01 15:35:16 -0500

    Chosse

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var phoneRouter = require('./routes/phone');
 var boardRouter = require('./routes/board');
+var ChooseRouter = require('./routes/Choose');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/phone', phoneRouter);
 app.use('/board', boardRouter);
+app.use('/Choose', ChooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
